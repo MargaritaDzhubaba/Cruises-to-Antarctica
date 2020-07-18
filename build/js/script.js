8647aed00b89ed85b207be597f1c41c2e7800041
MargaritaDzhubaba <rikajubaba@gmail.com>
2020-07-19 00:44:18 +0300

    Добавляет файл с зависимостями eslintrc, обновляет gitignore

--- build/js/script.js
+++ build/js/script.js
@@ -1,32 +1,15 @@
-'use strict'
+'use strict';
+var pageHeader = document.querySelector('.page-header');
+var headerToggle = document.querySelector('.page-header__toggle');
 
-const toggleBlockScroll = () => {
-  document.body.classList.toggle('scroll-hidden');
-};
+pageHeader.classList.remove('page-header--nojs');
 
-
-const header = document.querySelector('.page-header');
-const button = document.querySelector('.page-header__toggle');
-const nav = document.querySelector('.main-nav');
-const navButtons = document.querySelectorAll('.main-nav__item');
-
-const toogleMenu = () => {
-  header.classList.toggle('page-header--open');
-  header.classList.toggle('page-header--close');
-  toggleBlockScroll();
-};
-
-header.classList.remove('page-header--no-js');
-header.classList.add('page-header--close');
-nav.classList.remove('main-nav--no-js')
-nav.classList.add('main-nav--js');
-
-button.addEventListener('click', toogleMenu);
-
-navButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    if (header.classList.contains('page-header--open')) {
-      toogleMenu();
-    }
-  });
+headerToggle.addEventListener('click', function () {
+  if (pageHeader.classList.contains('page-header--closed')) {
+    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.add('page-header--opened');
+  } else {
+    pageHeader.classList.add('page-header--closed');
+    pageHeader.classList.remove('page-header--opened');
+  }
 });
